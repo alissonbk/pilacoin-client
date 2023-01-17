e83e3d5adaadf765a0676827fb587c974a6154cd
alissonbk <alissonbk2010@hotmail.com>
2023-01-17 11:25:49 -0300

    Layout home

--- src/app/components/principal/home/home.component.ts
+++ src/app/components/principal/home/home.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Usuario } from './../../../core/model/usuario.model';
+import { LoginService } from './../../../core/service/login.service';
+import { Component, OnInit } from '@angular/core';
+import { faCoins, faCopy, faWallet } from '@fortawesome/free-solid-svg-icons';
+import Swal from 'sweetalert2';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  faCoins = faCoins;
+  faWallet = faWallet;
+  faCopy = faCopy;
+  toast: any;
+  usuario: Usuario = new Usuario();
+
+  constructor(private loginService: LoginService) {
+    this.toast = Swal.mixin({
+      toast: true,
+      position: 'top-right',
+      iconColor: 'white',
+      customClass: {
+        popup: 'colored-toast'
+      },
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true
+    })
+  }
+
+  ngOnInit(): void {
+      this.loginService.loggedUser != null ? this.usuario = this.loginService.loggedUser : '';
+  }
+
+  copyClipboard(value: string) {
+    navigator.clipboard.writeText(value);
+    Swal.fire({
+      toast: true,
+      position: 'top-right',
+      icon: 'info',
+      title: 'Copiado!',
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true
+    })
+    }
 }
